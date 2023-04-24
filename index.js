6e152e55ad258be699f070798460b73c2108950d
Alph3ga <akashneelrc@gmail.com>
2023-04-25 03:32:09 +0530

    started on search

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 var express= require('express');
 var fs= require('fs').promises;
 var apiK= require('./bookApi');
+const fetch= require('node-fetch');
 
 var app= express();
 const host = '0.0.0.0';
 const port = 3000;
 
 const initialRequestListener= (req,res)=>{
-    fs.readFile(__dirname + "/res/html/index.html").then(
-        content=> {
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(content);
-        }
-    ).catch(err=>{
-        res.writeHead(500);
-        res.end(err);
-        return;
-    })
+    res.sendFile(__dirname+ '/res/html/index.html');
 };
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 app.get('/', initialRequestListener);
 app.use(express.static('res'));
+
+app.get('/search', (req, res)=>{
+    console.log(req.query.search_text);
+    var imurl;
+    apiK.getVolumesInfo(req.query.search_text).then(books=> imurl=(books[0].volumeInfo.imageLinks.thumbnail));
+});
+
 app.listen(port, host);
 
-apiK.getVolumesInfo('percy jackson').then(books=> console.log(books[0].volumeInfo.imageLinks));
